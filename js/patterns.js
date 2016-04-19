f3d2ec473f0a0d47bd252d5dc3b52a3b12568a85
Matthew Chamberlain <matthewwillliamchamberlain@gmail.com>
2016-04-20 11:47:06 +1200

    Added form validation

---
+++ js/patterns.js
@@ -0,0 +1,6 @@
+var namePattern = new RegExp("^[a-zA-Z.-]{2,30}$");
+var addressPattern = new RegExp("^[a-zA-Z0-9 .-]{5,50}$");
+var emailPattern = new RegExp("^[a-zA-Z0-9@ ._-]{5,50}$");
+var phonePatter = new RegExp("^[0-9 ]{5,50}$");
+var checkInPattern = new RegExp("");
+var checkOutPattern = new RegExp("");
--- js/validation.js
+++ js/validation.js
@@ -16,7 +16,8 @@ var email = document.querySelector("#email");
 var emailMessage = document.querySelector("#email-message");
 var phoneNumber = document.querySelector("#phone");
 var phoneMessage = document.querySelector("#phone-message");
-var tour = document.querySelector();
+var tour = document.querySelector("#tour");
+var tourMessage = document.querySelector("#tour-message");
 var checkIn = document.querySelector("#check-in");
 var checkOut = document.querySelector("#check-out");
 
@@ -25,5 +26,59 @@ firstName.onblur = function() {
 		firstNameMessage.innerHTML = "Your name must be at least two characters long";
 	} else if ( this.value.length > 30) {
 		firstNameMessage.innerHTML = "Your name can not be more than thirty characters long";
+	} else if ( namePattern.test ( this.value ) ) {
+		firstNameMessage.innerHTML = "";
 	}
+	 else {
+		firstNameMessage.innerHTML = "Invalid first name";
+	}
+}
+
+lastName.onblur = function() {
+	if( this.value.length < 2) {
+		lastNameMessage.innerHTML = "Your name must be at least two characters long";
+	} else if ( this.value.length > 30) {
+		lastNameMessage.innerHTML = "Your name can not be more than thirty characters long";
+	} else if ( namePattern.test ( this.value ) ) {
+		lastNameMessage.innerHTML = "";
+	}
+	 else {
+		lastNameMessage.innerHTML = "Invalid last name";
+	}
+}
+
+address.onblur = function() {
+	if( this.value.length < 5) {
+		addressMessage.innerHTML = "Your address must be at least five characters long";
+	} else if ( this.value.length > 50) {
+		addressMessage.innerHTML = "Your address can not be more than fifty characters long";
+	} else if ( addressPattern.test ( this.value ) ) {
+		addressMessage.innerHTML = "";
+	}
+	 else {
+		addressMessage.innerHTML = "Invalid address";
+	}
+}
+
+email.onblur = function() {
+	if( this.value.length < 5) {
+		addressMessage.innerHTML = "Your email address must be at least five characters long";
+	} else if ( this.value.length > 50) {
+		addressMessage.innerHTML = "Your email address can not be more than fifty characters long";
+	} else if ( emailPattern.test ( this.value ) ) {
+		addressMessage.innerHTML = "";
+	}
+	 else {
+		addressMessage.innerHTML = "Invalid email address";
+	}
+}
+
+tour.onchange = function() {
+
+	if( this.value == "" ) {
+		tourMessage.innerHTML = 'Please choose a tour';
+	} else {
+		tourMessage.innerHTML = '';
+	}
+
 }
