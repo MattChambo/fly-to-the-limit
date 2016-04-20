bd40eec6aa11eaa8162ff2874f3a84408e962b12
Matthew Chamberlain <matthewwillliamchamberlain@gmail.com>
2016-04-21 11:46:08 +1200

    Continue developing form validation

--- js/patterns.js
+++ js/patterns.js
@@ -1,6 +1,6 @@
-var namePattern = new RegExp("^[a-zA-Z.-]{2,30}$");
+//Regular expressions for validation.js
+
+var namePattern = new RegExp("^[a-zA-Z.'-]{2,30}$");
 var addressPattern = new RegExp("^[a-zA-Z0-9 .-]{5,50}$");
-var emailPattern = new RegExp("^[a-zA-Z0-9@ ._-]{5,50}$");
-var phonePatter = new RegExp("^[0-9 ]{5,50}$");
-var checkInPattern = new RegExp("");
-var checkOutPattern = new RegExp("");
+var emailPattern = new RegExp("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+var phonePatter = new RegExp("^[0-9 ()]{5,50}$");
--- js/validation.js
+++ js/validation.js
@@ -21,6 +21,8 @@ var tourMessage = document.querySelector("#tour-message");
 var checkIn = document.querySelector("#check-in");
 var checkOut = document.querySelector("#check-out");
 
+// Check to see if first name is correct length and contains only valid characters and display message to user if the name is invalid
+
 firstName.onblur = function() {
 	if( this.value.length < 2) {
 		firstNameMessage.innerHTML = "Your name must be at least two characters long";
@@ -29,10 +31,13 @@ firstName.onblur = function() {
 	} else if ( namePattern.test ( this.value ) ) {
 		firstNameMessage.innerHTML = "";
 	}
-	 else {
-		firstNameMessage.innerHTML = "Invalid first name";
+	else {
+		firstNameMessage.innerHTML = "Your first name cannot contain invalid characters";
 	}
 }
+
+// Check to see if last name is correct length and contains only valid characters and display message to user if the name is invalid
+
 
 lastName.onblur = function() {
 	if( this.value.length < 2) {
@@ -42,10 +47,12 @@ lastName.onblur = function() {
 	} else if ( namePattern.test ( this.value ) ) {
 		lastNameMessage.innerHTML = "";
 	}
-	 else {
-		lastNameMessage.innerHTML = "Invalid last name";
+	else {
+		lastNameMessage.innerHTML = "Your last name cannot contain invalid characters";
 	}
 }
+
+// Check to see if the address is correct length and contains only valid characters and display message to user if the address is invalid
 
 address.onblur = function() {
 	if( this.value.length < 5) {
@@ -55,23 +62,43 @@ address.onblur = function() {
 	} else if ( addressPattern.test ( this.value ) ) {
 		addressMessage.innerHTML = "";
 	}
-	 else {
-		addressMessage.innerHTML = "Invalid address";
+	else {
+		addressMessage.innerHTML = "Your address can only contain letters, numbers, spaces, dots and dashes";
 	}
 }
 
+// Check to see if email address is formated correctly, is an appropriate length and contains only valid characters and then display message to user if the email address is invalid
+
 email.onblur = function() {
 	if( this.value.length < 5) {
-		addressMessage.innerHTML = "Your email address must be at least five characters long";
+		emailMessage.innerHTML = "Your email address must be at least five characters long";
 	} else if ( this.value.length > 50) {
-		addressMessage.innerHTML = "Your email address can not be more than fifty characters long";
+		emailMessage.innerHTML = "Your email address can not be more than fifty characters long";
 	} else if ( emailPattern.test ( this.value ) ) {
-		addressMessage.innerHTML = "";
+		emailMessage.innerHTML = "";
 	}
-	 else {
-		addressMessage.innerHTML = "Invalid email address";
+	else {
+		emailMessage.innerHTML = "Invalid email address";
 	}
 }
+
+// Check to see if phone number is an appropriate length and contains only valid characters then display a message to the user if phone number is invalid
+
+phoneNumber.onblur = function() {
+	if( this.value.length < 5) {
+		phoneMessage.innerHTML = "Your phone number must be at least five characters long";
+	} else if ( this.value.length > 50) {
+		phoneMessage.innerHTML = "Your phone number must be less that fifty characters long";
+	} else if ( phonePattern.test ( this.value ) ) {
+		phoneMessage.innerHTML = "";
+	}
+	else {
+		phoneMessage.innerHTML = "You phone number should only contain numbers, spaces and brackets";
+	}
+
+}
+
+// Check to see that a tour has been selected and display a message if a tour has not been selected
 
 tour.onchange = function() {
 
@@ -80,5 +107,37 @@ tour.onchange = function() {
 	} else {
 		tourMessage.innerHTML = '';
 	}
+
+}
+
+bookingForm.onsubmit = function(event) {
+	var totalErrors = 0;
+	var allGenderOptions = document.querySelectorAll("[name=gender]")
+
+var genderIsSelected = false;
+//Loop over all the campus options
+	for(var i=0; i<allGenderOptions.length; i++) {
+
+  		//check if this campus has been selected
+  		if( allGenderOptions[i].checked ) {
+  			genderIsSelected = true;
+ 		}
+
+  	}
+
+ 	// If camousIsSelected is still false
+  	if( genderIsSelected == false ) {
+ 		genderMessage.innerHTML = "Please select a gender";
+  		totalErrors++;
+  		} else {
+  		campusMessage.innerHTML = "";
+  	}
+
+ 	//If the total errors is greater than 0
+ 	if( totalErrors > 0) {
+  		// Stop the form from submitting
+ 		event.preventDefault();
+  	}
+	
 
 }
